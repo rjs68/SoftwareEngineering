71413b0d033e90094a46828fa5403b8efa302e73
Rhys <rhys-stephens@hotmail.co.uk>
2020-01-20 13:30:28 +0000

    Minor changes to code to increase readability

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e/40bd4b5c883b001a1a9cdb01857b8538
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		PrintStream ps = new PrintStream(System.out);
+		
+		Person donald = new Person("Donald", new Date(2, 1, 1972));
+		donald.print(ps);
+		ps.println(donald.getName());
+		ps.println(donald.getAge());
+	}
+
+}
--- SELab2/PersonTest.java
+++ SELab2/PersonTest.java
@@ -3,11 +3,14 @@ import java.io.PrintStream;
 public class PersonTest {
 
 	public static void main(String[] args) {
+		//Create a PrintStream object
 		PrintStream ps = new PrintStream(System.out);
 		
-		Person donald = new Person("Donald", new Date(2, 1, 1972));
-		donald.print(ps);
-		ps.println(donald.getName());
+		Person donald = new Person("Donald", new Date(2, 1, 1972)); //create a new person
+		donald.print(ps); //test the print method
+		
+		//test the getters and print out the results
+		ps.println(donald.getName()); 
 		ps.println(donald.getAge());
 	}
 
